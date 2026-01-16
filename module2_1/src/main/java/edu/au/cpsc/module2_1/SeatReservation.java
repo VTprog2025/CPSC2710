0552c069f25d35eff475e5ab523727f3a8c733f7
Christopher Boartfield <clb0214@auburn.edu>
2026-01-16 17:22:35 -0600

    made better improvements to SeatReservation and app

--- module2_1/src/main/java/edu/au/cpsc/module2_1/SeatReservation.java
+++ module2_1/src/main/java/edu/au/cpsc/module2_1/SeatReservation.java
@@ -1,65 +1,53 @@
 package edu.au.cpsc.module2_1;
 
 import java.time.LocalDate;
-/*
-* Project: Module 2
-* Author: Christopher Boartfield
-* Email: clb0214@auburn.edu
-* Date: 1-16-2026
-* Description: Fight Reservation App
- */
+// * Project: Module 2
+// * Author: Christopher Boartfield
+// * Email: clb0214@auburn.edu
+// * Date: 1-16-2026
+// * Description: Fight Reservation App
 
 public class SeatReservation {
 
-    /* Private instance variables */
-    private String flightDesignator;
-    private LocalDate flightDate;
-    private String firstName;
-    private String lastName;
-    private int numberOfBags;
-    private boolean flyingWithInfant;
-    private boolean flyingWithTravelInsurance;
+    private String firstName = "";
+    private String lastName = "";
+    private String flightDesignator = "";
+    private LocalDate flightDate = LocalDate.now();
+    private int numberOfBags = 0;
+    private boolean flyingWithInfant = false;
+    private boolean flyingWithTravelInsurance = false;
 
-    /* First Name */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
     public String getFirstName() {
-        if (firstName == null) {
-            throw new IllegalArgumentException("First name cannot be null");
+        if (firstName == null || firstName.isEmpty()) {
+            throw new IllegalArgumentException("First name cannot be empty");
         }
-        if (firstName.length() < 1 || firstName.length() > 15) {
-            throw new IllegalArgumentException(
-                    "First name must be between 1 and 15 characters");
+        if (firstName.length() > 15) {
+            throw new IllegalArgumentException("First name must be between 1 and 15 characters");
         }
         return firstName;
     }
 
-    /* Last Name */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
     public String getLastName() {
-        if (lastName == null) {
-            throw new IllegalArgumentException("Last name cannot be null");
+        if (lastName == null || lastName.isEmpty()) {
+            throw new IllegalArgumentException("Last name cannot be empty");
         }
-        if (lastName.length() < 1 || lastName.length() > 15) {
-            throw new IllegalArgumentException(
-                    "Last name must be between 1 and 15 characters");
+        if (lastName.length() > 15) {
+            throw new IllegalArgumentException("Last name must be between 1 and 15 characters");
         }
         return lastName;
     }
 
-    /* Flight Designator */
     public void setFlightDesignator(String flightDesignator) {
-        if (flightDesignator == null ||
-                flightDesignator.length() < 4 ||
-                flightDesignator.length() > 6) {
-            throw new IllegalArgumentException(
-                    "Flight designator must be between 4 and 6 characters."
-            );
+        if (flightDesignator == null || flightDesignator.length() < 4 || flightDesignator.length() > 6) {
+            throw new IllegalArgumentException("Flight designator must be between 4 and 6 characters");
         }
         this.flightDesignator = flightDesignator;
     }
@@ -68,7 +56,14 @@ public class SeatReservation {
         return flightDesignator;
     }
 
-    /* Number of Bags */
+    public void setFlightDate(LocalDate flightDate) {
+        this.flightDate = flightDate;
+    }
+
+    public LocalDate getFlightDate() {
+        return flightDate;
+    }
+
     public void setNumberOfBags(int numberOfBags) {
         this.numberOfBags = numberOfBags;
     }
@@ -77,7 +72,6 @@ public class SeatReservation {
         return numberOfBags;
     }
 
-    /* Flying With Infant */
     public boolean isFlyingWithInfant() {
         return flyingWithInfant;
     }
@@ -90,7 +84,6 @@ public class SeatReservation {
         flyingWithInfant = false;
     }
 
-    /* Flying With Travel Insurance */
     public boolean hasTravelInsurance() {
         return flyingWithTravelInsurance;
     }
@@ -103,14 +96,6 @@ public class SeatReservation {
         flyingWithTravelInsurance = false;
     }
 
-    /* Flight Date */
-    public void setFlightDate(LocalDate flightDate) {
-        this.flightDate = flightDate;
-    }
-
-    public LocalDate getFlightDate() {
-        return flightDate;
-    }
     @Override
     public String toString() {
         return "SeatReservation {" +
@@ -123,5 +108,4 @@ public class SeatReservation {
                 ", flyingWithTravelInsurance=" + flyingWithTravelInsurance +
                 '}';
     }
-
 }
