898fdd5ed7739f83f2864306215bfc4745d304f0
Christopher Boartfield <clb0214@auburn.edu>
2026-01-16 16:12:53 -0600

    Made Changes to SeatReservation

---
+++ module2_1/src/main/java/edu/au/cpsc/module2_1/SeatReservation.java
@@ -0,0 +1,36 @@
+package edu.au.cpsc.module2_1;
+
+import java.time.LocalDate;
+/*
+* Project: Module 2
+* Author: Christopher Boartfield
+* Email: clb0214@auburn.edu
+* Date: 1-16-2026
+* Description: Fight Reservation App
+ */
+
+public class SeatReservation {
+
+    /* Private instance variables*/
+    private String flightDesignator;
+    private LocalDate flightDate;
+    private String firstName;
+    private String lastName;
+
+    // setter for flightDesignator and fullfills module1 part 2 assignment.
+    public void setFlightDesignator(String flightDesignator) {
+        if (flightDesignator == null ||
+                flightDesignator.length() < 4 ||
+                flightDesignator.length() > 6) {
+            throw new IllegalArgumentException(
+                    "Flight designator must be between 4 and 6 characters."
+            );
+        }
+        this.flightDesignator = flightDesignator;
+    }
+
+    /*
+     * Module1 assignment.
+     */
+}
+
