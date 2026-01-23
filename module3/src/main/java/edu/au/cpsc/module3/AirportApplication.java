f427efcb3c9b0e6757af5ce7275f7e5abaf40469
Christopher Boartfield <clb0214@auburn.edu>
2026-01-23 16:10:01 -0600

    Modified

--- module3/src/main/java/edu/au/cpsc/module3/AirportApplication.java
+++ module3/src/main/java/edu/au/cpsc/module3/AirportApplication.java
@@ -8,12 +8,22 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class AirportApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AirportApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                AirportApplication.class.getResource("airport-view.fxml")
+        );
+
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+
+        stage.setTitle("Airport Data");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
