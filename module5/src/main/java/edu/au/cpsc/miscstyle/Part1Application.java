69a0a76931a3c1ee0e35d142ce65d82835c85bab
Christopher Boartfield <clb0214@auburn.edu>
2026-02-07 10:36:22 -0600

    changes and added notes

--- module5/src/main/java/edu/au/cpsc/miscstyle/Part1Application.java
+++ module5/src/main/java/edu/au/cpsc/miscstyle/Part1Application.java
@@ -4,13 +4,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
+
+/*
+Project: project5
+Author: Christopher Boartfield
+auburn email: clb0214@auburn.edu
+Date:2/7/2026
+Description: Project5 controller.
+ */
+
 public class Part1Application extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new
                 FXMLLoader(Part1Application.class.getResource("part1.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Part 1");
+        stage.setTitle("Christopher Boartfields Flight App");
         stage.setScene(scene);
         stage.show();
     }
--- module5/src/main/java/edu/au/cpsc/miscstyle/Part1Controller.java
+++ module5/src/main/java/edu/au/cpsc/miscstyle/Part1Controller.java
@@ -1,5 +1,13 @@
 package edu.au.cpsc.miscstyle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+/*
+* Project: project5
+* Author: Christopher Boartfield
+* auburn email: clb0214@auburn.edu
+* Date:2/7/2026
+* Description: Project5 controller.
+ */
 public class Part1Controller {
 }
