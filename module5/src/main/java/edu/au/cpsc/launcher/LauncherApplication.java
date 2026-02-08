7df9983962f61e113bf044327b122e6165b2d822
Christopher Boartfield <clb0214@auburn.edu>
2026-02-08 14:44:51 -0600

    this is a mess and I hate it

--- module5/src/main/java/edu/au/cpsc/launcher/LauncherApplication.java
+++ module5/src/main/java/edu/au/cpsc/launcher/LauncherApplication.java
@@ -2,24 +2,26 @@ package edu.au.cpsc.launcher;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class LauncherApplication extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(
-                LauncherApplication.class.getResource("launcher-app.fxml")
-        );
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/launcher/launcher-app.fxml"));
+        Parent root = loader.load();
 
-        Scene scene = new Scene(loader.load(), 600, 400);
-        stage.setTitle("Flight Management Launcher");
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Launcher App");
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(600);  // Adjust as needed
+        primaryStage.setHeight(400); // Adjust as needed
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
