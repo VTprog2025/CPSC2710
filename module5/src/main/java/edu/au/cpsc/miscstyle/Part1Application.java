f631ab947831350a0dd5782284174028e121e2f2
Christopher Boartfield <clb0214@auburn.edu>
2026-02-08 21:27:28 -0600

    this is still a mess and I still hate it

--- module5/src/main/java/edu/au/cpsc/miscstyle/Part1Application.java
+++ module5/src/main/java/edu/au/cpsc/miscstyle/Part1Application.java
@@ -1,10 +1,10 @@
 package edu.au.cpsc.miscstyle;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
-
 /*
 Project: project5
 Author: Christopher Boartfield
@@ -16,23 +16,21 @@ Description: Project5 controller.
 public class Part1Application extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-                getClass().getResource("/edu/au/cpsc/miscstyle/part1.fxml")
-        );
+    public void start(Stage primaryStage) throws Exception {
+        // Load FXML from resource folder
+        Parent root = FXMLLoader.load(getClass().getResource("/edu/au/cpsc/miscstyle/part1.fxml"));
 
+        Scene scene = new Scene(root);
 
-        Scene scene = new Scene(fxmlLoader.load());
+        // Load CSS from resource folder
+        scene.getStylesheets().add(getClass().getResource("/edu/au/cpsc/miscstyle/main.css").toExternalForm());
 
-        // Attach CSS explicitly here
-        scene.getStylesheets().add(getClass().getResource("/edu/au/cpsc/miscstyle/style/main.css").toExternalForm());
-
-        stage.setTitle("Christopher Boartfield's Flight Designator App");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("YourName YourLastName's Flight Designator App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
