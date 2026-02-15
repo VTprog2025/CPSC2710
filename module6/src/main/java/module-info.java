cb1fedb3101950da968beac0efbad750e17e0e95
Christopher Boartfield <clb0214@auburn.edu>
2026-02-15 09:51:58 -0600

    keeps wanting me to commit this workspace file

--- module6/src/main/java/module-info.java
+++ module6/src/main/java/module-info.java
@@ -1,8 +1,10 @@
-module edu.au.cpsc.module6 {
+module edu.au.cpsc.module {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens edu.au.cpsc.module6 to javafx.fxml;
+    exports edu.au.cpsc.part1;
     exports edu.au.cpsc.module6;
+
+    opens edu.au.cpsc.part1 to javafx.fxml;
+    opens edu.au.cpsc.module6 to javafx.fxml;
 }
